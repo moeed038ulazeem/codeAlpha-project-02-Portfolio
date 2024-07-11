d617f822e4972f4236c3d64ba717e68160e9a6a3
moeed038ulazeem <moeedmoeed038@gamil.com>
2024-07-11 00:43:17 -0700

    project-02-Portfolio

---
+++ java.js
@@ -0,0 +1,5 @@
+//  form submission handling
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    console.log('Form submitted!');
+});
